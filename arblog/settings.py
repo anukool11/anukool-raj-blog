3134f388236d66f85cb6c37ea15c91125c886323
anukool11 <66172267+anukool11@users.noreply.github.com>
2020-11-12 01:31:04 +0530

    Update settings.py

--- arblog/settings.py
+++ arblog/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
+
+STATIC_ROOT="/root/myproj/site/public/static"
